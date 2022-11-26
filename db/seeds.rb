cddd6017616d1bce8850b4985f1ba7d5aa021cc7
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-11-26 11:36:44 +0300

    Add model and migration

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+
+Greeting.destroy_all
+Greeting.create({greeting: "Hi"})
+Greeting.create({greeting: "Hello"})
+Greeting.create({greeting: "what's up"})
+Greeting.create({greeting: "Hi Hi"})
+Greeting.create({greeting: "Hi Hi Hi"})
